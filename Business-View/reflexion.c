2039bda5915317ad1942996e6df738be76fad5ec
Loan Bernat UBUNTU <loantravail@gmail.com>
2021-11-30 14:51:36 +0100

    reflexion comparaison

--- Business-View/reflexion.c
+++ Business-View/reflexion.c
@@ -5,8 +5,29 @@
 
 
 
-//DESCRIPTEUR
+//DESCRIPTEUR TEXTE
+Texte :
+	- Num ID : ?Mais pk le num id au lieu de l'adresse du fichier'
+	- listeTerme
+	- Nb Terme (taille de la liste)
+	- Taille du texte (une fois traitee)
+
+Termes :
+	- Terme next
+	- mot
+	- nb d'occurence'
+
+//DESCRIPTEUR IMAGE
+Image :
+	- Num ID :
 
 
 
-//
+//DESCRIPTEUR AUDIO
+Audio:
+	- Num ID :
+	- Matrice N*N int 
+
+
+//Comparaison
+Fichier liste base contenant lesn oms des fichiers et leurs ID.
