e327c50048ceb0746d5c786264a086fdbbe7462e
Loan Bernat UBUNTU <loantravail@gmail.com>
2021-12-09 13:56:58 +0100

    Petites Modifications

--- Dev/include/recherche.h
+++ Dev/include/recherche.h
@@ -1,30 +1,34 @@
 #ifndef RECHERCHE_H
 #define RECHERCHE_H 
 
-typedef DESCRIPTEUR_AUDIO* LISTE_DESCRIPTEUR_AUDIO;
-typedef DESCRIPTEUR_IMAGE* LISTE_DESCRIPTEUR_IMAGE;
-typedef DESCRIPTEUR_TEXTE* LISTE_DESCRIPTEUR_TEXTE;
+typedef DESCRIPTEUR_AUDIO* PILE_DESCRIPTEUR_AUDIO;
+typedef DESCRIPTEUR_IMAGE* PILE_DESCRIPTEUR_IMAGE;
+typedef DESCRIPTEUR_TEXTE* PILE_DESCRIPTEUR_TEXTE;
 
 /*
 Permet d'ouvrir un fichier selon son type
 */
 int ouvrirFichier(FILE* fichier);
 
+PILE_DESCRIPTEUR_AUDIO recupPileDescripteurAudio();
 
+PILE_DESCRIPTEUR_TEXTE recupPileDescripteurTexte();
+
+PILE_DESCRIPTEUR_IMAGE recupPileDescripteurImage();
 
 //---------------------- FONCTION DE RECHERCHE SPECIALISE -------------------------
 
 //Effectue une recherche via un mot cle
-LISTE_DESCRIPTEUR_TEXTE rechercheTexteParMotCle(char* mot_cle);
+PILE_DESCRIPTEUR_TEXTE rechercheTexteParMotCle(char* mot_cle);
 
 //Effectue une recherche via un descripteur de fichier texte
-LISTE_DESCRIPTEUR_TEXTE rechercheTexteparDescripteur(DESCRIPTEUR_TEXTE descripteur_texte_cible);
+PILE_DESCRIPTEUR_TEXTE rechercheTexteparDescripteur(DESCRIPTEUR_TEXTE descripteur_texte_cible);
 
 //Effectue une recherche via un descripteur de fichier image
-LISTE_DESCRIPTEUR_IMAGE rechercheImageParDescripteur(DESCRIPTEUR_IMAGE descripteur_image_cible);
+PILE_DESCRIPTEUR_IMAGE rechercheImageParDescripteur(DESCRIPTEUR_IMAGE descripteur_image_cible);
 
 //Effectue une recherche via un descripteur de fichier Audio
-LISTE_DESCRIPTEUR_AUDIO rechercheAudioParDescripteur(DESCRIPTEUR_AUDIO descripteur_audio_cible);
+PILE_DESCRIPTEUR_AUDIO rechercheAudioParDescripteur(DESCRIPTEUR_AUDIO descripteur_audio_cible);
 
 
 //----------------------- FONCTION APPELLE DEPUIS L'INTERFACE ----------------------
