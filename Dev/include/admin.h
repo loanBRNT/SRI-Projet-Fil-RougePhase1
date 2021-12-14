6e7ac5bc0dc030af2326e463287711c116409846
Loan Bernat UBUNTU <loantravail@gmail.com>
2021-12-14 11:22:24 +0100

    Prototype Accesseur et modifieur

--- Dev/include/admin.h
+++ Dev/include/admin.h
@@ -8,6 +8,7 @@ typedef struct s_config{
 	int nbIntervalle;
 	int nbFenetre;
 	int nbBits;
+	int maj; //vaut 0 a chaque ouverture. Et mit a 1 si un des attributs subit un changement.
 }*PTR_CONFIG;
 
 //Permet de mettre en memoire
@@ -18,24 +19,39 @@ PTR_CONFIG ouvrirPanneauDeConfiguration();
 int affectAttributConfig(PTR_CONFIG config, char* nomAttribut, int valeur);
 
 //Permet de sauvegarder Apres modif
-void sauvegarderConfig();
+void sauvegarderConfig(PTR_CONFIG config);
+
+//libere l'espace memoire
+void fermetureConfig(PTR_CONFIG config);
 
 // ---------------- PERMET DE CHANGER DES ATTRIBUTS DU .CONFIG ---------------
-//Renvoie un message d'etat.
+//Renvoie 0 s'il y a eu une erreur, 1 sinon.
 
-char* changerTauxSimmilaritude(int taux);
+int changerTauxSimmilaritude(PTR_CONFIG config, int nb);
 
-char* changerNbMotParTexte(int nb);
+int changerNbMotParTexte(PTR_CONFIG config, int nb);
 
-char* changerSeuilOccurenceMot(int nb);
+int changerSeuilOccurenceMot(PTR_CONFIG config, int nb);
 
-char* changerNbIntervalle(int nb);
+int changerNbIntervalle(PTR_CONFIG config, int nb);
 
-char* changerNbFenetre(int nb);
+int changerNbFenetre(PTR_CONFIG config, int nb);
 
-char* changerNbBits(int nb);
+int changerNbBits(PTR_CONFIG config, int nb);
 
 
 //-------------------- ACCESSEUR ---------------------------
+
+int recupTauxSimmilaritudeDuConfig();
+
+int recupNbMotParTexteDuConfig();
+
+int recupSeuilOccurenceMotDuConfig();
+
+int recupNbIntervalleDuConfig();
+
+int recupNbFenetreDuConfig();
+
+int recupNbBitsDuConfig();
 
 #endif
--- Dev/src/admin.c
+++ Dev/src/admin.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include "../include/admin.h"
 
+/* 
+ ----------------------- Signature -------------------------
+|															|
+|		Auteur : BERNAT Loan								|
+|		Date de creation : 10/12/21							|
+|		Date de derniere MAJ : xx/01/22						|
+|															|
+ ----------------------------------------------------------- 
+ */
+
 int affectAttributConfig(PTR_CONFIG config, char* nomAttribut, int valeur){
 	if (!strcmp(nomAttribut,"tauxSim")){
 		config->tauxSim = valeur;
@@ -33,6 +43,8 @@ PTR_CONFIG ouvrirPanneauDeConfiguration(){
 		fprintf(stderr, "ERREUR : probleme d'acces a .config");
 		return NULL;
 	}
+	
+	// On parcourt chaque ligne du .config et on lest stocke en memoire a l'adresse pointe par config
 
 	do {
 		fscanf(ptr_fic,"%s %d", nomAttribut, &valeur);
@@ -40,13 +52,50 @@ PTR_CONFIG ouvrirPanneauDeConfiguration(){
 			fprintf(stderr,"ERREUR : Parametre %s du .config inconu",nomAttribut);
 		}
 	} while (!feof(ptr_fic));
+	fclose(ptr_fic);
 
+	config->maj = 0;
 	return config;
+}
 
+void sauvegardeConfig(PTR_CONFIG config){
+	FILE * ptr_fic = fopen(".config","w");
+	if (ptr_fic == NULL){
+		fprintf(stderr, "ERREUR : probleme d'acces a .config");
+	} else {
+		fprintf(ptr_fic, "tauxSim %d\nnbMaxMotParTexte %d\nseuilOccurenceMot %d\nnombreIntervalleAudio %d\nnombreFenetreAudio %d\nnombreBitsQuantification %d",
+		config->tauxSim, config->nbMotParTxt, config->seuilOccMot, config->nbIntervalle, config->nbFenetre, config->nbBits);
+	}
+	fclose(ptr_fic);
+}
+
+void fermetureConfig(PTR_CONFIG config){
+	if (config->maj == 1){
+		sauvegardeConfig(config);
+	}
+	free(config);
+}
+
+int changerTauxSimmilaritude(PTR_CONFIG config, int nb){
+	if (config != NULL) {
+		if ((nb > 0) && (nb <= 100)){
+			config->tauxSim = nb;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int recupTauxSimmilaritudeDuConfig(){
+
+	//utilisation d'une fonction generale pour parcourir et avec 2 param recup la bonne ligne
+
+	return 0;
 }
 
 
 void main(void)
 {
 	PTR_CONFIG config = ouvrirPanneauDeConfiguration();
+	fermetureConfig(config);
 }
