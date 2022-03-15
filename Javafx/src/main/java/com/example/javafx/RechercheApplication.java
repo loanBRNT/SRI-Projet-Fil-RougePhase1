269811bbf08013a20c09b1bb6a156657acc4ab39
Damien <83353387+Dams66@users.noreply.github.com>
2022-03-15 19:34:31 +0100

    Add files via upload

--- Javafx/src/main/java/com/example/javafx/RechercheApplication.java
+++ Javafx/src/main/java/com/example/javafx/RechercheApplication.java
@@ -3,6 +3,7 @@ package com.example.javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class RechercheApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(RechercheApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 450);
         stage.setTitle("Bienvenue");
+        stage.getIcons().add(new Image("file:1f50d.png"));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
